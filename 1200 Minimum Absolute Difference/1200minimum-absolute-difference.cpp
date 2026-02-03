f52e8f4ca4f08c7191248ecab7eaf4963c27be37
Vanshmiglani007 <vanshmiglani29707@gmail.com>
2026-02-03 10:34:26 +0000

    Code challenge submission for question: 1200 Minimum Absolute Difference

---
+++ 1200 Minimum Absolute Difference/1200minimum-absolute-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        int diff = INT_MAX;
+        for (int i = 1; i < arr.size(); i++) {
+            diff = min(diff, arr[i] - arr[i - 1]);
+        }
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] == diff) {
+                ans.push_back({arr[i - 1], arr[i]});
+            }
+        }
+
+        return ans;
+    }
+};
