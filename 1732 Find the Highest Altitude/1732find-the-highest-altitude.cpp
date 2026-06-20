87af149a00c9e956fc1934a426b9a11b6437d447
Vanshmiglani007 <vanshmiglani29707@gmail.com>
2026-06-20 09:20:58 +0000

    Code challenge submission for question: 1732 Find the Highest Altitude

---
+++ 1732 Find the Highest Altitude/1732find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n =gain.size();
+        vector<int>ans(n+1,0);
+        for(int i=0;i<n;i++){
+            ans[i+1]=ans[i]+gain[i];
+        }
+        int maxi = *max_element(ans.begin(),ans.end());
+        return maxi;
+    }
+};
