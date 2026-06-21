58a392de179b0bc45efd1df298e2968a6b692419
Vanshmiglani007 <vanshmiglani29707@gmail.com>
2026-06-21 17:08:56 +0000

    Code challenge submission for question: 55 Jump Game

---
+++ 55 Jump Game/55jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int count=0;
+        for(int i =0;i<nums.size();i++){
+            if(i>count) {
+                return false;
+            }
+
+            count=max(count,i+nums[i]);
+        }
+        return true;
+    }
+};
