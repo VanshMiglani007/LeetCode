6922d2863a2e989fda2a2d83cdc6db09dc6198b5
Vansh <24BCS11961@cuchd.in>
2026-07-29 19:06:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52.28%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+
+        for (char c : s) {
+            if (c >= '0' && c <= '9') {
+                if (!ans.empty())
+                    ans.pop_back();
+            } else {
+                ans.push_back(c);
+            }
+        }
+
+        return ans;
+    }
+};
