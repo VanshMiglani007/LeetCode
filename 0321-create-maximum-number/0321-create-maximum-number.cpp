9a5f53ee1608c1ecd179f52954f153d84f12a39c
Vansh <24BCS11961@cuchd.in>
2026-09-16 11:01:32 +0530

    Time: 23 ms (73.68%), Space: 32.4 MB (29.19%) - LeetHub

--- 0321-create-maximum-number/0321-create-maximum-number.cpp
+++ 0321-create-maximum-number/0321-create-maximum-number.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
-    vector<int> getMax(vector<int>& nums, int k) {
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
         vector<int> st;
-        int drop = nums.size() - k;
-
+        int remove = nums.size() - k;
         for (int x : nums) {
-            while (!st.empty() && drop && st.back() < x) {
+            while (!st.empty() && remove > 0 && st.back() < x) {
                 st.pop_back();
-                drop--;
+                remove--;
             }
             st.push_back(x);
         }
-
         st.resize(k);
         return st;
     }
-
-    bool greater(vector<int>& a, int i, vector<int>& b, int j) {
-        while (i < a.size() && j < b.size() && a[i] == b[j]) {
+    bool greater(vector<int>& a, int i,vector<int>& b, int j) {
+        while (i < a.size() && j < b.size()) {
+            if (a[i] != b[j])
+                return a[i] > b[j];
             i++;
             j++;
         }
-        return j == b.size() || (i < a.size() && a[i] > b[j]);
+        return (a.size() - i) > (b.size() - j);
     }
-
     vector<int> merge(vector<int>& a, vector<int>& b) {
-        vector<int> res;
-        int i = 0, j = 0;
-
+        vector<int> result;
+        int i = 0;
+        int j = 0;
         while (i < a.size() || j < b.size()) {
-            if (greater(a, i, b, j))
-                res.push_back(a[i++]);
-            else
-                res.push_back(b[j++]);
+            if (greater(a, i, b, j)) {
+                result.push_back(a[i]);
+                i++;
+            }
+            else {
+                result.push_back(b[j]);
+                j++;
+            }
         }
-
-        return res;
+        return result;
     }
+    vector<int> maxNumber(vector<int>& nums1,vector<int>& nums2,int k) {
+        vector<int> ans;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int start = max(0, k - n2);
+        int end = min(k, n1);
 
-    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<int> ans(k, 0);
-
-        int low = max(0, k - (int)nums2.size());
-        int high = min(k, (int)nums1.size());
-
-        for (int x = low; x <= high; x++) {
-            vector<int> a = getMax(nums1, x);
-            vector<int> b = getMax(nums2, k - x);
-            vector<int> cur = merge(a, b);
-
-            if (greater(cur, 0, ans, 0))
-                ans = cur;
+        for (int i = start; i <= end; i++) {
+            vector<int> a = maxSubsequence(nums1, i);
+            vector<int> b = maxSubsequence(nums2, k - i);
+            vector<int> candidate = merge(a, b);
+            if (ans.empty() || greater(candidate, 0, ans, 0)) {
+                ans = candidate;
+            }
         }
-
         return ans;
     }
 };
